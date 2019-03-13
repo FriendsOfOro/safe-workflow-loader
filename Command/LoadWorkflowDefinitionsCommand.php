8a98bee227b011014e1ad1ec6706578214bc912e
Maxime BONGIORNO <maxime@kiboko.fr>
2019-03-13 11:50:10 +0100

    add dry-run option

--- Command/LoadWorkflowDefinitionsCommand.php
+++ Command/LoadWorkflowDefinitionsCommand.php
@@ -5,12 +5,24 @@ namespace Hackoro\Bundle\WorkflowLoader\Command;
 use Oro\Bundle\WorkflowBundle\Command\LoadWorkflowDefinitionsCommand as OroLoadWorkflowDefinitionsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
 
 /**
  * Decorates the original oro:workflow:definitions:load command
  */
 class LoadWorkflowDefinitionsCommand extends OroLoadWorkflowDefinitionsCommand
 {
+
+    protected function configure()
+    {
+        parent::configure();
+        $this->addOption(
+            'dry-run',
+            '-d',
+            InputOption::VALUE_OPTIONAL);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // TODO
